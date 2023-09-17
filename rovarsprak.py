a48e6b88d0a30ac6275ce373e3a403fdc8be922e
Johan Eskils <eskils@telia.com>
2023-09-17 09:28:13 +0200

    commit

--- rovarsprak.py
+++ rovarsprak.py
@@ -1,12 +1,8 @@
-from rs_funktioner import text_imp
-from rs_funktioner import rs_transl
-from rs_funktioner import text_sav
-
-
-#______Main______
-
-
 def rovare(fil_r, prefix = "rs_", frc = 0.5):
+
+    from rs_funktioner import text_imp
+    from rs_funktioner import rs_transl
+    from rs_funktioner import text_sav
 
  #   check_encode('rovarsprak_svenskt_text.txt')
     text_r, fil_r, fil_n = text_imp(fil_r, frc)
@@ -14,10 +10,6 @@ def rovare(fil_r, prefix = "rs_", frc = 0.5):
     text_sav(fil_r, fil_n, rsprak, prefix)
 
     return rsprak
-
-fil_r = 'german.txt'
-
-rovare(fil_r,'AA')
 
 #felhantering
 #path fel
