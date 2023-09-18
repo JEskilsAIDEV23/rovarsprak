9eb363d4af0d173769ae04bec93652ccb45d0876
Johan Eskils <eskils@telia.com>
2023-09-18 11:09:08 +0200

    commit

--- check_txt2.py
+++ check_txt2.py
@@ -3,24 +3,21 @@ from check import check_encode
 fil_r = input('Läs fil: ')
 encode = check_encode(fil_r)
 txt_r = ""
+if encode == 'cp1252':
+ #   enc = 'UTF-8'
+    enc = encode
 
-with open(fil_r, 'r', encoding = 'UTF') as f:
+with open(fil_r, 'r', encoding = enc) as f:
     for i in f:
         i = i.replace('\t', '')
         i = i.replace('\n', '')
         txt_r = txt_r + i + '\n'
 
-    n = 0
-    for j in txt_r:
-        if n > 50 and n < 60:
-            print(n, j)
-        n = n+1
+    fil_s = fil_r + ' (from ' +str(enc)+ ' to UTF-8).txt'
 
-    fil_s = fil_r + ' (from ' +str(encode)+ ' to UTF-8).txt'
+    comment = f'Filen lästes som {enc}\n'
 
-    comment = f'Filen lästes som "UTF"\n'
-
-    print(f'Filen lästes som "UTF"')
+    print(f'Filen lästes som {enc}')
 
 with open(fil_s, 'w', encoding = 'UTF-8') as sav:
     sav.write(comment+'\n')
